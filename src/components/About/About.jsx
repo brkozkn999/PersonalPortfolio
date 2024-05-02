b2a9408e51650eb54751404608f4d0c8942fc917
ibrahimberkozkan <ibrahimberkozkan34@gmail.com>
2024-05-02 20:08:42 +0300

    initial commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About Me:</h2>
+      <div className={styles.content}>
+        <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="Me"
+          className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Who's Berk?👨🏻‍💻</h3>
+              <p>
+              I am a recently graduated computer programming student. I aim to improve myself 1 step more every day by building various projects, mainly backend.🎯
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Backend Developer🗄️</h3>
+              <p>
+                I have experience developing fast and optimised back-end systems
+                and API's
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/uiIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer🖼️</h3>
+              <p>
+                I'm still improving my skills in Frontend through various projects.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
